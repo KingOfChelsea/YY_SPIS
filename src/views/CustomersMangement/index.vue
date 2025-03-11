deeafef5d20f8b7f1ca75fcad64748ee9cbf1020
KingOfChelsea <xuzhenyu3333@gmail.com>
2025-03-11 15:13:43 +0800

    创建三个节点文件，客户管理，订单报价，订单管理
    Signed-off-by: KingOfChelsea <xuzhenyu3333@gmail.com>

---
+++ src/views/CustomersMangement/index.vue
@@ -0,0 +1,11 @@
+<template>
+<el-text>客户管理</el-text>
+</template>
+
+<script setup >
+
+</script>
+
+<style>
+
+</style>
---
+++ src/views/SalesQuotation/index.vue
@@ -0,0 +1,18 @@
+<!--
+ * @Description:
+ * @Author: Zane Xu
+ * @Date: 2025-03-11 15:11:30
+ * @LastEditTime: 2025-03-11 15:12:02
+ * @LastEditors: Zane Xu
+-->
+<template>
+    <el-text>销售报价</el-text>
+</template>
+
+<script setup lang='ts'>
+
+</script>
+
+<style>
+
+</style>
