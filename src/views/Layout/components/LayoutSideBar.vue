198939af149e4ffbb2f038927f319b5514123a69
KingOfChelsea <xuzhenyu3333@gmail.com>
2025-03-12 17:19:29 +0800

    节点展开维护
    Signed-off-by: KingOfChelsea <xuzhenyu3333@gmail.com>

--- src/views/Layout/components/LayoutSideBar.vue
+++ src/views/Layout/components/LayoutSideBar.vue
@@ -1,45 +1,59 @@
 <template>
-  <el-menu default-active="2" class="el-menu-vertical-demo">
+  <el-menu
+    class="el-menu-vertical-demo"
+    :unique-opened="true"
+    :default-openeds="openedMenus"
+  >
     <div v-for="(Fitem, Findex) in SideBarStore.SideBarList" :key="Findex">
-      <div>
-        <el-sub-menu index="1">
-          <template #title>
-            <el-icon><location /></el-icon>
-            <span>{{ Fitem.PermissionName }}</span>
-          </template>
-          <div>
-            <el-menu-item-group :key="Findex + 1" :title="Fitem.PermissionName">
-              <div v-for="(item, index) in Fitem.SubMenus" :key="index">
-                <el-menu-item :index="`1-${index}`" @click="goToPage(item)">
-                  {{ item.PermissionName }}
-                </el-menu-item>
-              </div>
-            </el-menu-item-group>
-          </div>
-        </el-sub-menu>
-      </div>
+      <el-sub-menu
+        :index="`${Findex + 1}`"
+        @click="toggleMenu(Findex + 1)"
+      >
+        <template #title>
+          <el-icon><location /></el-icon>
+          <span>{{ Fitem.PermissionName }}</span>
+        </template>
+        <el-menu-item-group :key="Findex + 1" :title="Fitem.PermissionName">
+          <el-menu-item
+            v-for="(item, index) in Fitem.SubMenus"
+            :key="index"
+            :index="`${Findex + 1}-${index + 1}`"
+            @click="goToPage(item)"
+          >
+            {{ item.PermissionName }}
+          </el-menu-item>
+        </el-menu-item-group>
+      </el-sub-menu>
     </div>
   </el-menu>
 </template>
 
-<script setup lang="ts">
-import { useSideBarStore } from "@/stores/sidebar";
-import { onMounted } from "vue";
+<script setup>
+import { ref, onMounted } from "vue";
 import { useRouter } from "vue-router";
+import { useSideBarStore } from "@/stores/sidebar";
 
 const router = useRouter();
 const SideBarStore = useSideBarStore();
+const openedMenus = ref([]); // 记录当前展开的菜单项
 
 onMounted(async () => {
-  SideBarStore.getSideBar();
+  await SideBarStore.getSideBar();
 });
 
-// 菜单项点击事件处理函数
+// **菜单点击时控制展开**
+const toggleMenu = (index) => {
+  if (openedMenus.value.includes(index)) {
+    openedMenus.value = openedMenus.value.filter(i => i !== index); // 关闭已展开的菜单
+  } else {
+    openedMenus.value = [index]; // 仅展开当前菜单
+  }
+};
+
+// **跳转页面**
 const goToPage = (item) => {
   if (item.Path) {
-    router.push(`${item.Path}`); // 跳转到目标路由
+    router.push(`${item.Path}`);
   }
 };
 </script>
-
-<style lang="scss"></style>
